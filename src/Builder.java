d35df4f7e11e285f1080b538b113b814da5cc240
Ian Hecker <ianhecker@yahoo.com>
2019-02-05 16:18:44 -0700

    testing simple java driver using non-deprecated APIs

--- src/Builder.java
+++ src/Builder.java
@@ -6,94 +6,32 @@ THIS IS FOR LATER
 
  */
 
-import org.antlr.v4.runtime.ANTLRInputStream;
-import org.antlr.v4.runtime.CharStream;
-import org.antlr.v4.runtime.CommonTokenStream;
-import org.antlr.v4.runtime.TokenStream;
+import org.antlr.v4.runtime.*;
 
-import java.io.*;
-import java.util.Scanner;
-import java.util.regex.*;
-import java.util.*;
+import java.io.File;
 
 public class Builder {
-    public static void main(String[] args) {
 
-        File sourceCode = new File(System.getProperty("user.dir").replace("/src", "") + "/Step1/inputs/sqrt.micro");
-        PrintStream out = null;
-        File tokensFile = new File(System.getProperty("user.dir") + "/LittleGrammarLexer.tokens");
-        String tokenString = fileToString(tokensFile);
-        try {
-            out = new PrintStream(System.getProperty("user.dir") + "/OUTPUT.txt");
+    public static void main(String[] args) throws Exception {
+        String filePath = "null";
 
-        String contents = fileToString(sourceCode);
-
-        CharStream input = new ANTLRInputStream(contents);
-        LittleGrammarLexer lexer = new LittleGrammarLexer(input);
-
-        //System.out.println("Tokens: " + lexer.getAllTokens() + "#");
-
-        out.print(lexer.getAllTokens());
-        out.close();
-
-        File out2 = new File(System.getProperty("user.dir") + "/OUTPUT.txt");
-        String contents2 = fileToString(out2);
-
-        Pattern patternToken = Pattern.compile("(.*)=([0-9]+)");
-        Matcher matcherToken = patternToken.matcher(tokenString);
-
-        Pattern pattern = Pattern.compile("'([a-z]*[A-Z]*[0-9]*)',<([0-9]*)>");
-        Matcher matcher = pattern.matcher(contents2);
-
-
-
-        ArrayList<String> tokenReference = new ArrayList<String>();
-
-        tokenReference.add(null);
-        while (matcherToken.find()) {
-            tokenReference.add(matcherToken.group(1));
-
+        if (args.length > 0) {
+            //System.out.println(args[0]);
+            //args[0] is the next string after the name of the Java file:
+            //$ java Builder apple -> agrs[0] == apple
+            filePath = args[0];
         }
-
-        out.close();
-        out = new PrintStream(System.getProperty("user.dir") + "/OUTPUT.txt");
-
-        while(matcher.find()) {
-
-            //Divide matcher into correct groups
-            String token = matcher.group(1);
-            String tokenType = matcher.group(2);
-
-            //Print to console
-            System.out.println("Token type: " + tokenReference.get(Integer.parseInt(tokenType)) + "\nValue: " + token);
-
-            out.print("Token type: " + tokenReference.get(Integer.parseInt(tokenType)) + "\nValue: " + token + "\n\n");
-
-            System.out.println();
-        } } catch (FileNotFoundException fnfe) {
-            System.out.println("File does not exist.");
+        else{
+            System.out.println("File Path not provided");
         }
+        //Create a char stream for input into the lexer
+        CharStream cs = CharStreams.fromFileName(filePath);//input string here
+        //Create a lexer using the compiled (name of file)lexer.class from the .g4 file
+        LittleGrammarLexer lexer = new LittleGrammarLexer(cs);
+        //Create a token stream from output of the lexer
+        CommonTokenStream tokens = new CommonTokenStream(lexer);
 
-        //TokenStream tokens = new CommonTokenStream(lexer);
-        //ExprParser parser = new ExprParser(tokens);
-        //ExprParser.ExpressionContext uu = parser.expression();
-        //for (int i = 0; i < uu.getChildCount(); ++i)
-        //    System.out.println(uu.getChild(i));
-    }
-
-    private static String fileToString(File file) {
-        try {
-            StringBuilder contents = new StringBuilder((int) file.length());
-
-            try (Scanner in = new Scanner(file)) {
-                while (in.hasNextLine()) {
-                    contents.append(in.nextLine() + System.lineSeparator());
-                }
-                return contents.toString();
-            }
-        } catch (IOException ioe) {
-            System.out.println("IOException");
-        }
-        return null;
+        //Use the parser on the tokens to check the semantics
+        //LittleGrammarParser parser = new LittleGrammarParser(tokens);
     }
 }
