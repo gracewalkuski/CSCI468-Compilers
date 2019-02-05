b4dab9e693d18614e507e2504ac713f9c0303da4
Ian Hecker <ianhecker@yahoo.com>
2019-02-05 16:32:37 -0700

    Modified Builder.java

--- src/Builder.java
+++ src/Builder.java
@@ -20,16 +20,19 @@ public class Builder {
             //args[0] is the next string after the name of the Java file:
             //$ java Builder apple -> agrs[0] == apple
             filePath = args[0];
+
+            //Create a char stream for input into the lexer
+            CharStream cs = CharStreams.fromFileName(filePath);//input string here
+            //Create a lexer using the compiled (name of file)lexer.class from the .g4 file
+            LittleGrammarLexer lexer = new LittleGrammarLexer(cs);
+            //Create a token stream from output of the lexer
+            CommonTokenStream tokens = new CommonTokenStream(lexer);
+            System.out.println(tokens);
         }
         else{
             System.out.println("File Path not provided");
         }
-        //Create a char stream for input into the lexer
-        CharStream cs = CharStreams.fromFileName(filePath);//input string here
-        //Create a lexer using the compiled (name of file)lexer.class from the .g4 file
-        LittleGrammarLexer lexer = new LittleGrammarLexer(cs);
-        //Create a token stream from output of the lexer
-        CommonTokenStream tokens = new CommonTokenStream(lexer);
+
 
         //Use the parser on the tokens to check the semantics
         //LittleGrammarParser parser = new LittleGrammarParser(tokens);
