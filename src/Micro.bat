6487fd04cc69bffb497a914b5f370225e5b82eb4
Ian Hecker <ianhecker@yahoo.com>
2019-02-05 17:25:59 -0700

    Micro now uses Antlr.jar to compile grammar files, and compiles LittleGrammar java files

--- src/Micro.bat
+++ src/Micro.bat
@@ -1,7 +1,10 @@
 ::This is the batch file that compiles all java files, and runs the java
 ::Driver file Builder.java with the four provided .micro files
+java -jar ../lib/antlr-4.7.2-complete.jar LittleGrammar.g4
 
+javac LittleGrammar*.java
 javac Builder.java
+
 java Builder ../inputs/sqrt.micro
 java Builder ../inputs/fibonacci.micro
 java Builder ../inputs/loop.micro
