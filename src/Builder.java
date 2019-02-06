de8a4586ec76aa4cb9201395928acde8c4a8da4e
Ian Hecker <ianhecker@yahoo.com>
2019-02-05 17:06:25 -0700

    Java Builder able to print out lexer tokens correctly

--- src/Builder.java
+++ src/Builder.java
@@ -8,8 +8,6 @@ THIS IS FOR LATER
 
 import org.antlr.v4.runtime.*;
 
-import java.io.File;
-
 public class Builder {
 
     public static void main(String[] args) throws Exception {
@@ -26,8 +24,9 @@ public class Builder {
             //Create a lexer using the compiled (name of file)lexer.class from the .g4 file
             LittleGrammarLexer lexer = new LittleGrammarLexer(cs);
             //Create a token stream from output of the lexer
-            CommonTokenStream tokens = new CommonTokenStream(lexer);
-            System.out.println(tokens);
+            //CommonTokenStream tokens = new CommonTokenStream(lexer);
+
+            printTokens(lexer);
         }
         else{
             System.out.println("File Path not provided");
@@ -36,5 +35,31 @@ public class Builder {
 
         //Use the parser on the tokens to check the semantics
         //LittleGrammarParser parser = new LittleGrammarParser(tokens);
+    }
+
+    private static void printTokens(LittleGrammarLexer l){
+        //Create TokenSource from lexer
+        TokenSource tokenSource = l;
+        //Get Lexer vocabulary
+        Vocabulary v = l.getVocabulary();
+
+        //Iterate through all tokens from lexer
+        while (true) {
+            Token t = tokenSource.nextToken();
+            //If token is last one from file, then end
+            if (t.getType() == Token.EOF) {
+                break;
+            }
+
+            //Get symbolic name from lexer vocabulary of specific token
+            String tokenType = v.getSymbolicName(t.getType());
+            //Get value of specific token
+            String tokenValue = t.getText();
+
+            //Print out tokenType and tokenValue
+            System.out.printf("Token Type: %s\n", tokenType);
+            System.out.printf("Value: %s\n", tokenValue);
+        }
+
     }
 }
