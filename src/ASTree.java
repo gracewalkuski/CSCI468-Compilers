7ef85469e4990921bdb27fa49eebd39177bfe378
Jakob Frank <jfbcchs@gmail.com>
2019-04-21 18:00:53 -0600

    removed useless ASTree methods

--- src/ASTree.java
+++ src/ASTree.java
@@ -9,46 +9,6 @@ class ASTree {
         this.root = node;
     }
 
-    public void insert(ASTNode node) {
-        this.root = insertRecursively(root, node);
-    }
-
-    private ASTNode insertRecursively(ASTNode currNode, ASTNode newNode) {
-        if (currNode == null) {
-            return newNode;
-        }
-        if (currNode.leftChild == null) {
-
-        }
-        else if (currNode.rightChild == null) {
-            currNode.rightChild = newNode;
-        }
-        else {
-            System.out.println("Node not added. Already has left and right child.");
-            return currNode;
-        }
-        return currNode;
-    }
-
-    public void insertLeftChild(ASTNode node) {
-        if (this.root.leftChild == null) {
-            this.root.leftChild = node;
-        }
-        else {
-            System.out.println("THIS NODE ALREADY HAS A LEFT CHILD");
-        }
-    }
-
-    public void insertRightChild(ASTNode node) {
-        if (this.root.rightChild == null) {
-            this.root.rightChild = node;
-        }
-        else {
-            System.out.println("THIS NODE ALREADY HAS A RIGHT CHILD");
-        }
-    }
-
-
     void postOrderTraversal(ASTNode node) {
         if (node == null) return;
 
